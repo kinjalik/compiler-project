8021f8277156818bd84858e65ecf51bbec472314
Albert <albert@kinjalik.ru>
2023-02-18 18:31:14 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.8.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(19))
+    }
+}
+
+kotlin {
+    jvmToolchain {
+        (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(19))
+    }
+}
