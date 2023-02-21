c44e6cea813fa96e613cd13a252f784beaafce60
Mark Esaian <qwert11223381@gmail.com>
2023-02-22 00:38:19 +0300

    Component test for Lexer

--- src/main/java/org/example/tokens/Token.kt
+++ src/main/java/org/example/tokens/Token.kt
@@ -1,5 +1,14 @@
 package org.example.tokens
 
 abstract class Token {
+
+    /**
+     * Returns describing representation of the token
+     */
     abstract override fun toString(): String
+
+    /**
+     * Returns the code represented by this token
+     */
+    abstract fun reverse(): String
 }
---
+++ src/test/java/ExampleProgramArgumentProvider.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ExampleProgramArgumentProvider implements ArgumentsProvider {
+    final String EXAMPLE_FOLDER = "examples";
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+        ClassLoader classLoader = getClass().getClassLoader();
+        String path = classLoader.getResource("examples").getPath();
+        var fileListStream = Stream.of(new File(path).listFiles())
+                .filter(file -> !file.isDirectory())
+                .map(File::getAbsolutePath)
+                .map(Arguments::of);
+       return fileListStream;
+    }
+}
