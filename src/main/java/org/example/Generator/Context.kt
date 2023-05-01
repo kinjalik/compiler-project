1c0fd125f8d51abcffa8577552612617d8022bfe
Markuu-s <qwert11223381@gmail.com>
2023-05-01 19:30:15 +0300

    Add context.

--- src/main/java/org/example/Generator/Context.kt
+++ src/main/java/org/example/Generator/Context.kt
@@ -1,4 +1,18 @@
 package org.example.Generator
 
-class Context {
+class Context(var idCounter: Int) {
+    var nameByNum: MutableMap<Int, String> = mutableMapOf()
+    private val numByName: MutableMap<String, Int> = mutableMapOf()
+    var isProg: Boolean = false
+
+    fun getAtomAddr(name: String): Pair<Int, Boolean> {
+        var isAdded = false
+        if (!numByName.contains(name)) {
+            isAdded = true
+            numByName[name] = idCounter
+            nameByNum[idCounter] = name
+            idCounter += 1
+        }
+        return Pair(numByName[name]?.times(32) ?: -1, isAdded)
+    }
 }
